79421c32191d76f4159334d1f0910106ae70582b
foluke agbede <folukeagbede@icloud.com>
2021-05-10 11:22:51 +0100

    added logic for floor conversion from American to European

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const toAmericanFloor = number => {
-  return 0;
+
+ //if floor number is less that 0 i.e. floor number is negative don't change floor number
+if(number < 0){
+  return number;
+}
+
+//if floor number is greater than 13 decrease num by 2 
+if(number > 13){
+  return number - 2;
+}
+
+if(number == 0){ 
+  return number;
+}
+  //if floor num is between 1 and 12 decrease num by 1
+if(number >1 || number <13 ){
+  return number-1
+}
+
 }
 
 export default toAmericanFloor;
--- src/index.test.js
+++ src/index.test.js
@@ -8,4 +8,13 @@ describe('Converting the lift floor', () => {
   it('should return zero for the first floor', () => {
     expect(toAmericanFloor(1)).toStrictEqual(0);
   })
+
+  it('should return floor num -2 if floor above 13', () => {
+    expect(toAmericanFloor(15)).toStrictEqual(13);
+  })
+
+  it('check basement floor remains the same', () => {
+    expect(toAmericanFloor(-3)).toStrictEqual(-3);
+  })
+
 });

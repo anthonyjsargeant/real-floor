32639846d7ef29f3d56376e4573f5d313905fef4
chrl0t <charlotte_i@live.co.uk>
2021-05-11 11:00:14 +0100

    Write test for if something other than a number is passed in

--- src/index.test.js
+++ src/index.test.js
@@ -12,10 +12,18 @@ describe("Converting the lift floor", () => {
   it("should return the same floor if number is less than 1", () => {
     expect(toAmericanFloor(-1)).toStrictEqual(-1);
   });
+
   it("should return four for the fifth floor", () => {
     expect(toAmericanFloor(5)).toStrictEqual(4);
   });
+
   it("should return fifteen for the seventeeth floor", () => {
     expect(toAmericanFloor(17)).toStrictEqual(15);
+  });
+
+  it("should return an error message if a value other than a number is passed in", () => {
+    expect(toAmericanFloor("Hiya")).toStrictEqual(
+      "Please enter a valid number"
+    );
   });
 });

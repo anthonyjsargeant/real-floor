4a1ed95111c31f41241e0e28ce12def250c79cca
chrl0t <charlotte_i@live.co.uk>
2021-05-11 10:46:33 +0100

    Write  test for negative input

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,15 @@
 import toAmericanFloor from "./index";
 
-describe('Converting the lift floor', () => {
-  it('should return zero for the ground floor', () => {
+describe("Converting the lift floor", () => {
+  it("should return zero for the ground floor", () => {
     expect(toAmericanFloor(0)).toStrictEqual(0);
   });
 
-  it('should return zero for the first floor', () => {
+  it("should return zero for the first floor", () => {
     expect(toAmericanFloor(1)).toStrictEqual(0);
-  })
+  });
+
+  it("should return the same floor if number is less than 1", () => {
+    expect(toAmericanFloor(-1)).toStrictEqual(-1);
+  });
 });

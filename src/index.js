c7506b7ed46245f6eae726e26db0f63d1c4f2c4a
jakecolder93 <jakecolder93@gmail.com>
2021-05-12 14:07:46 +0100

    if statement for toAmericanFloor function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const toAmericanFloor = number => {
-  return 0;
+
+  if(number <= 0){
+      return number
+  } else if (0 < number < 13){
+      return number = number -1
+  } else if (13 <= number ){
+      return number = number -2
+  }
 }
+
 
 export default toAmericanFloor;
